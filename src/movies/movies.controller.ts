ce1384d4c296f0e7c72c2f471a3df6d3ea3869c8
suhyeok-luke <rlatn219@naver.com>
2022-09-23 17:10:44 +0900

    add DTO

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Param, Post, Delete, Put, Patch, Body, Query } from '@nestjs/common';
+import { Controller, Get, Param, Post, Delete, Put, Patch, Body, Query, Req, Res } from '@nestjs/common';
+import { CreateMovieDTO } from './dto/create-movie.dto';
+import { UpdateMovieDTO } from './dto/update-movie.dto';
 import { Movie } from './entities/movies.entity';
 import { MoviesService } from './movies.service';
 
@@ -18,22 +20,23 @@ export class MoviesController {
     }
 
     @Get(':id') // 얘 밑에 있으면 다 Get의 param을 id로 인식함
-    getOne(@Param('id') movieId: string) : Movie {
+    getOne(@Param('id') movieId: number) : Movie {
+        console.log(typeof movieId)
         return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData){
+    create(@Body() movieData: CreateMovieDTO){
         return this.moviesService.create(movieData)
     }
 
     @Delete(":id")
-    remove(@Param('id') movieId: string){
+    remove(@Param('id') movieId: number){
         return this.moviesService.deleteOne(movieId);
     }
 
     @Patch(':id')
-    patch(@Param('id') movieId: string, @Body() updateData){
+    patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDTO){
         return this.moviesService.update(movieId, updateData);
     }
 
